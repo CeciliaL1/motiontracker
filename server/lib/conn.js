424e5ec9b7f454f3d72e141ffb9cc5581aecf202
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-12-25 11:25:46 +0100

    :sparkles: [server] adding local connection to database and the deployed

--- server/lib/conn.js
+++ server/lib/conn.js
@@ -1,12 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "Cecilia",
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+  host: "cecilial.hemsida.eu/",
+  port: "3000",
+  user: "Cecilia",
+  // password: process.env.DATABASE_PASSWORD,
+  //database: process.env.DATABASE_NAME,
+  // LOCAL
+  password: process.env.SQL_LOQAL_PASS,
+  database: process.env.SQL_LOQAL_DB_NAME,
 });
-
 
 module.exports = connection;
