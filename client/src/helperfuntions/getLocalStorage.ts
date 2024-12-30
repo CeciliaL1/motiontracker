93da1454ff2bb3df5423ab02a1668e78686abfed
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-12-30 10:40:13 +0100

    :sparkles: [client] helperfunctions for localstorage

---
+++ client/src/helperfuntions/getLocalStorage.ts
@@ -0,0 +1,4 @@
+export const getLocalStorage = async <T>(stored: string): Promise<T> => {
+    const storedObject = await JSON.parse(localStorage.getItem(`${stored}`) || '[]');
+    return storedObject
+}
