e6b3bc5eaec5db6b5430597dc5e6ac6c77ea7b5d
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-01-08 11:23:17 +0100

    :sparkles: [client] generatebutton for workouts

--- client/src/components/styled/styledButtons.tsx
+++ client/src/components/styled/styledButtons.tsx
@@ -11,7 +11,7 @@ export const PrimaryButton = styled.button<IButtonProps>`
   width: 350px;
   margin-top: ${(props) => props.margintop}px;
   margin-bottom: ${(props) => props.marginbottom}px;
-
+  font-family: "Raleway", serif;
   border: 1px solid transparent;
 
   font-size: 1em;
@@ -29,6 +29,7 @@ export const SecondaryButton = styled(PrimaryButton)`
   margin-top: ${(props) => props.margintop}px;
   background-color: #f2f1eb;
   border: 1px solid #88ab8e;
+  font-family: "Raleway", serif;
 
   &:hover {
     background-color: #dbe3db;
@@ -36,5 +37,21 @@ export const SecondaryButton = styled(PrimaryButton)`
   a {
     color: black;
     text-decoration: none;
+  }
+`;
+
+export const GenerateButton = styled.button`
+  height: 90px;
+  width: 250px;
+  border-radius: 30%;
+  background-color: #88ab8e;
+  font-family: "Raleway", serif;
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  cursor: pointer;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  i {
+    margin-left: 10px;
   }
 `;
--- client/src/views/GenerateWorkout.tsx
+++ client/src/views/GenerateWorkout.tsx
@@ -1,3 +1,15 @@
+import { GenerateButton } from "../components/styled/styledButtons";
+import { Wrapper } from "../components/styled/Wrappers";
+
 export const GenerateWorkout = () => {
-  return <>Generate Workout</>;
+  return (
+    <>
+      <Wrapper direction="row" margintop={13} marginleft={0}>
+        <GenerateButton>
+          Generate<i className="fa-solid fa-wand-magic-sparkles"></i>
+        </GenerateButton>
+      </Wrapper>
+      <Wrapper direction="row" margintop={5}></Wrapper>
+    </>
+  );
 };
