0c3b3bfe7ead35f34b3175eac53f49326ebf48fa
CeciliaL1 <cecilia.lepik@hotmail.com>
2025-01-16 10:36:51 +0100

    :sparkles: [client] handle render of signle workout and button to complete/unmark workout

--- client/src/components/PreviewSingleWorkout.tsx
+++ client/src/components/PreviewSingleWorkout.tsx
@@ -1,84 +1,123 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { IUserLogin } from "../models/IUsers";
-import { IWorkout, IWorkoutScheduele } from "../models/IWorkout";
+import { IWorkoutScheduele } from "../models/IWorkout";
 import { putData } from "../services/serviceBase";
 import { PrimaryButton, SecondaryButton } from "./styled/styledButtons";
 import { Heading1 } from "./styled/styledTextContent";
 import { Wrapper } from "./styled/Wrappers";
 import { ActionWorkoutType } from "../reducers/workoutReducer";
 import { WorkoutContext } from "../context/WorkoutContext";
+import { Ul } from "./styled/styledUl";
+import { IResponse } from "../models/IUserProfile";
 
 interface ISingleWorkoutProps {
-  workout: IWorkout | undefined;
+  workoutSchedule: IWorkoutScheduele;
   date: string | undefined;
   token: string;
   loggedInUser: IUserLogin;
-  workoutSchedule: IWorkoutScheduele;
 }
 
 export const PreviewSingleWorkout = ({
-  workout,
+  workoutSchedule,
   date,
   token,
   loggedInUser,
-  workoutSchedule,
 }: ISingleWorkoutProps) => {
+  const [message, setMessage] = useState("");
   const { dispatch } = useContext(WorkoutContext);
-  if (!workout) {
-    return;
+
+  if (!date || !workoutSchedule[date]) {
+    return null;
   }
 
-  const completeWorkout = async () => {
-    if (workout) {
-      workout.done = true;
-    }
+  const workoutsForDate = workoutSchedule[date];
+
+  const completeAllWorkoutsForDate = async () => {
+    const updatedWorkouts = workoutsForDate.map((work) => ({
+      ...work,
+      done: true,
+    }));
+
+    const updatedSchedule = { ...workoutSchedule, [date]: updatedWorkouts };
+
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     };
 
-    const response = await putData<object, string>(
+    const response = await putData<object, IResponse>(
       `https://cecilial.hemsida.eu/api/workout/update/${loggedInUser.userId}`,
-      { workoutSchedule },
+      { workoutSchedule: updatedSchedule },
       headers
     );
 
-    console.log(response);
-    dispatch({ type: ActionWorkoutType.TOGGLE, payload: workoutSchedule });
+    setMessage(response.message);
+    setTimeout(() => {
+      setMessage("");
+    }, 1500);
+
+    dispatch({ type: ActionWorkoutType.TOGGLE, payload: updatedSchedule });
   };
 
-  const unCompleteWorkout = async () => {
-    if (workout) {
-      workout.done = false;
-    }
+  const unCompleteAllWorkoutsForDate = async () => {
+    const updatedWorkouts = workoutsForDate.map((work) => ({
+      ...work,
+      done: false,
+    }));
+
+    const updatedSchedule = { ...workoutSchedule, [date]: updatedWorkouts };
+
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     };
 
-    const response = await putData<object, string>(
+    const response = await putData<object, IResponse>(
       `https://cecilial.hemsida.eu/api/workout/update/${loggedInUser.userId}`,
-      { workoutSchedule },
+      { workoutSchedule: updatedSchedule },
       headers
     );
-    console.log(response);
+    setMessage(response.message);
+    setTimeout(() => {
+      setMessage("");
+    }, 1500);
 
-    dispatch({ type: ActionWorkoutType.TOGGLE, payload: workoutSchedule });
+    dispatch({ type: ActionWorkoutType.TOGGLE, payload: updatedSchedule });
   };
+
   return (
     <>
       <Wrapper direction="column" margintop={5} marginbottom={20}>
+        {message && (
+          <>
+            <div aria-live="assertive">
+              <p id="error-message">{message}</p>
+            </div>
+          </>
+        )}
         <Wrapper direction="row" margintop={2} gap={100} marginbottom={7}>
           <Heading1>{date}</Heading1>
-          <PrimaryButton aria-label="Workout completed" onClick={completeWorkout}>
-            Workout completed
+          <PrimaryButton
+            aria-label="MMark workout as completed"
+            onClick={completeAllWorkoutsForDate}
+          >
+            Mark workout as completed
           </PrimaryButton>
-          <SecondaryButton aria-label="Unmark completed workout" onClick={unCompleteWorkout}>
-            Unmark workout
+          <SecondaryButton
+            aria-label="Unmark workout"
+            onClick={unCompleteAllWorkoutsForDate}
+          >
+            Unmark Workout
           </SecondaryButton>
         </Wrapper>
         <div>
-          {workout.task} - {workout.repetition}
+          {workoutsForDate.map((work, index) => (
+            <Ul key={index}>
+              <li>
+                {work.task} {work.repetition}
+              </li>
+            </Ul>
+          ))}
         </div>
       </Wrapper>
     </>
